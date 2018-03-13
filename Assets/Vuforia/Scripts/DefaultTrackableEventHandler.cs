09341a3cedd6748323d35003d3c108edd8f2c400
Alexander Bohlen <arbohlen@gmail.com>
2018-03-13 00:51:35 -0700

    Additional Assets and Working Video

--- Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
+++ Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
@@ -8,19 +8,21 @@ Confidential and Proprietary - Protected under copyright and other laws.
 
 using UnityEngine;
 using Vuforia;
+using UnityEngine.Video;
 
 /// <summary>
 ///     A custom handler that implements the ITrackableEventHandler interface.
 /// </summary>
 public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
 {
-    #region PRIVATE_MEMBER_VARIABLES
+    #region PROTECTED_MEMBER_VARIABLES
 
     protected TrackableBehaviour mTrackableBehaviour;
+	public VideoPlayer videoPlayer;
 
-    #endregion // PRIVATE_MEMBER_VARIABLES
+    #endregion // PROTECTED_MEMBER_VARIABLES
 
-    #region UNTIY_MONOBEHAVIOUR_METHODS
+    #region UNITY_MONOBEHAVIOUR_METHODS
 
     protected virtual void Start()
     {
@@ -29,7 +31,13 @@ public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandle
             mTrackableBehaviour.RegisterTrackableEventHandler(this);
     }
 
-    #endregion // UNTIY_MONOBEHAVIOUR_METHODS
+    protected virtual void OnDestroy()
+    {
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.UnregisterTrackableEventHandler(this);
+    }
+
+    #endregion // UNITY_MONOBEHAVIOUR_METHODS
 
     #region PUBLIC_METHODS
 
@@ -65,7 +73,7 @@ public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandle
 
     #endregion // PUBLIC_METHODS
 
-    #region PRIVATE_METHODS
+    #region PROTECTED_METHODS
 
     protected virtual void OnTrackingFound()
     {
@@ -84,6 +92,8 @@ public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandle
         // Enable canvas':
         foreach (var component in canvasComponents)
             component.enabled = true;
+
+		videoPlayer.Play ();
     }
 
 
@@ -106,5 +116,5 @@ public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandle
             component.enabled = false;
     }
 
-    #endregion // PRIVATE_METHODS
+    #endregion // PROTECTED_METHODS
 }
